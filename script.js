488fe8807bfd3b0f48bde9c7d0d7ee16c44eb0a8
EllaUsik <usik.ella@gmail.com>
2019-02-20 16:20:17 +0300

    fixed script

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-var sendButton = document.getElementsByClassName ('send')[0];
-console.log(sendButton);
-var textArea = document.getElementsByClassName ('your-message');
-console.log(textArea);
-var enteredText = document.getElementsByClassName ('your-message')[0].value;
-
+var sendButtonElement = document.getElementsByClassName ('send')[0];
+var textAreaElements = document.getElementsByClassName ('your-message');
+var MIN_SIZE = 4;
 function validateTextValue() {
   var enteredText = document.getElementsByClassName ('your-message')[0].value;
-  if (enteredText.length > 4) {
-    console.log('normal message');
+  if (enteredText.length > MIN_SIZE) {
+    console.log('message is approved');
   } else {
-    
-    console.log('is it message?');
+    console.log('very short message, add more information');
   }
-
   var enteredEmail = document.getElementsByClassName ('your-email')[0].value;
-  if (enteredEmail.length > 4) {
+  if (enteredEmail.length > MIN_SIZE) {
     console.log('normal email');
   } else {
-    console.log('check email');
+    console.log('check entered email');
   }
 }
-
-sendButton.addEventListener("click", validateTextValue);
+sendButtonElement.addEventListener("click", validateTextValue);
