d50eea2630047c8c530a4237289b4d36afe959dc
EllaUsik <usik.ella@gmail.com>
2019-02-21 11:11:47 +0300

    hide error mesages

--- script.js
+++ script.js
@@ -5,13 +5,21 @@ var sendButtonElement = document.getElementsByClassName ('send')[0];
 function validateTextValue() {
   var enteredText = document.getElementsByClassName ('your-message')[0].value;
   var enteredEmail = document.getElementsByClassName ('your-email')[0].value;
+  var showMessageErrorMesage = document.getElementById('errormessage');
+  var showEmailErrorMesage = document.getElementById('erroremail');
 
   if (enteredText.length < MIN_SIZE) {
-    var showMessageErrorMesage = document.getElementById('errormessage').classList.remove('hidden');
+    showMessageErrorMesage.classList.remove('hidden');
+  }
+  else {
+    showMessageErrorMesage.classList.add('hidden');
   }
 
   if (enteredEmail.length < MIN_SIZE) {
-    var showEmailErrorMesage = document.getElementById('erroremail').classList.remove('hidden');
+    showEmailErrorMesage.classList.remove('hidden');
+  }
+  else {
+    showEmailErrorMesage.classList.add('hidden');
   }
 }
 
