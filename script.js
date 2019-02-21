c887fe4899fabc419118043f3d799c26faa5c3d2
EllaUsik <usik.ella@gmail.com>
2019-02-21 10:08:57 +0300

    fixed script

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
+var MIN_SIZE = 4;
+
 var sendButtonElement = document.getElementsByClassName ('send')[0];
 var textAreaElements = document.getElementsByClassName ('your-message');
-var MIN_SIZE = 4;
+
 function validateTextValue() {
   var enteredText = document.getElementsByClassName ('your-message')[0].value;
+  var enteredEmail = document.getElementsByClassName ('your-email')[0].value;
+
   if (enteredText.length > MIN_SIZE) {
     console.log('message is approved');
   } else {
     console.log('very short message, add more information');
   }
-  var enteredEmail = document.getElementsByClassName ('your-email')[0].value;
+
   if (enteredEmail.length > MIN_SIZE) {
     console.log('normal email');
   } else {
     console.log('check entered email');
   }
 }
-sendButtonElement.addEventListener("click", validateTextValue);
+
+sendButtonElement.addEventListener('click', validateTextValue);
